c988aeb7e360e3c40fe0d1a6adaabf51546697a4
youjeongcha <97075247+youjeongcha@users.noreply.github.com>
2023-03-20 10:08:22 +0900

    교재 210 ~ 215 페이지  6.8.1

---
+++ LoginServletTest.java
@@ -0,0 +1,69 @@
+package scTest.ex01;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletConfig;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class LoginServlet3
+ */
+@WebServlet("/loginTest")
+public class LoginServletTest extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+	/**
+	 * @see Servlet#init(ServletConfig)
+	 */
+	public void init(ServletConfig config) throws ServletException {
+		System.out.println("init 메소드 호출");
+	}
+
+	/**
+	 * @see Servlet#destroy()
+	 */
+	public void destroy() {
+		System.out.println("destory 메소드 호출");
+	}
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		request.setCharacterEncoding("utf-8");
+		response.setContentType("text/html;charset=utf-8");
+		
+		PrintWriter out = response.getWriter();
+		
+		String user_id = request.getParameter("user_id");
+		String user_pw = request.getParameter("user_pw");
+		
+		System.out.println("아이디 : " + user_id);
+		System.out.println("패스워드 : " + user_pw);
+		
+		
+		if (user_id!= null && ( user_id.length() != 0))
+		{
+			out.print("<html>");
+			out.print("<body>");
+			out.print(user_id + "님!! 로그인 하셨습니다.");
+			out.print("</body>");
+			out.print("</html>");
+		}
+		else
+		{
+			out.print("<html>");
+			out.print("<body>");
+			out.print("아이디를 입력하세요!!!");
+			out.print("<br>");
+			out.print("<a href='http://localhost:8070/Ch06/login01_h.html'> 로그인 창으로 이동 </a>");
+			out.print("</body>");
+			out.print("</html>");
+		}
+	}
+}
